390d2809d278f2a9fed6868f886dacb2bd92e5a0
CristinaMPerez <114082381+CristinaMPerez@users.noreply.github.com>
2022-10-05 16:56:01 +0200

    ejercicio 04

---
+++ app/src/main/java/com/crismperez/pmdm/ut01/ex03/Ut01Ex03Activity.kt
@@ -0,0 +1,11 @@
+package com.crismperez.pmdm.ut01.ex03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ut01Ex03Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex03)
+    }
+}
---
+++ app/src/main/java/com/crismperez/pmdm/ut01/ex04/Ut01Ex04Activity.kt
@@ -0,0 +1,11 @@
+package com.crismperez.pmdm.ut01.ex04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ut01Ex04Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex04)
+    }
+}
